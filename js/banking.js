42fcf5c4aa58aae56dfa878d6b35ef04f04dc074
MiaBacchu <jewelranabacchu@gmail.com>
2021-08-17 01:05:05 +0600

    all added

--- js/banking.js
+++ js/banking.js
@@ -1,3 +1,4 @@
+// 1
 function getDepositValue(inputId) {
     // get deposit value
     const inputField = document.getElementById(inputId);
@@ -6,30 +7,36 @@ function getDepositValue(inputId) {
     inputField.value = '';
     return newAmount;
 }
+// 2
 function getTotal(totalId, newAmount) {
     const total = document.getElementById(totalId)
     let previousTotal = parseFloat(total.innerText);
     total.innerText = previousTotal + newAmount;
 }
-
+// 3
+function totalBalance(balaceId, isAdd) {
+    const balanceTotal = document.getElementById(balaceId);
+    let previousBalanceTotal = parseFloat(balanceTotal.innerText);
+    if (isAdd == true) { balanceTotal.innerText = previousBalanceTotal + newDepositAmount; }
+    else { balanceTotal.innerText = previousBalanceTotal - newWithdrawAmount; }
+}
+// deposit
 document.getElementById('deposit-button').addEventListener('click', function () {
     // call the function
-    newDepositAmount = getDepositValue('deposit-input')
-    // get balance total
-    getTotal('deposit-total', newDepositAmount)
-    const balanceTotal = document.getElementById('balance-total');
-    let previousBalanceTotal = parseFloat(balanceTotal.innerText);
-    // adding balance total
-    balanceTotal.innerText = previousBalanceTotal + newDepositAmount;
-
+    newDepositAmount = getDepositValue('deposit-input');
+    // call get total
+    getTotal('deposit-total', newDepositAmount);
+    // call balanceTotal
+    totalBalance('balance-total', true)
 })
 
 // withdraw
 
 document.getElementById('withdraw-button').addEventListener('click', function () {
-    newWithdrawAmount = getDepositValue('withdraw-input')
-    getTotal('withdraw-total', newWithdrawAmount)
-    const balanceTotal = document.getElementById('balance-total');
-    const previousBalanceTotal = parseFloat(balanceTotal.innerText)
-    balanceTotal.innerText = previousBalanceTotal - newWithdrawAmount;
+    newWithdrawAmount = getDepositValue('withdraw-input');
+    getTotal('withdraw-total', newWithdrawAmount);
+    totalBalance('balance-total', false)
+    // const balanceTotal = document.getElementById('balance-total');
+    // const previousBalanceTotal = parseFloat(balanceTotal.innerText)
+    // balanceTotal.innerText = previousBalanceTotal - newWithdrawAmount;
 })
